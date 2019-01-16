c27b8580060bf32a523dbf62788004c26f5f9e74
Martin Schnabel <mb0@mb0.org>
2019-01-16 21:17:12 +0100

    lex: add more common predicates and two sequence splitters

--- lex/preds.go
+++ lex/preds.go
@@ -29,6 +29,11 @@ func IsNamePart(r rune) bool {
 	return IsLetter(r) || IsDigit(r) || r == '_'
 }
 
+// IsSign tests whether r is a plus or minus sign
+func IsSign(r byte) bool {
+	return r == '+' || r == '-'
+}
+
 // IsName tests whether s is a valid name
 func IsName(s string) bool {
 	if len(s) == 0 || !IsLetter(rune(s[0])) {
@@ -40,4 +45,39 @@ func IsName(s string) bool {
 		}
 	}
 	return true
+}
+
+// IsTag tests whether s starts with a colon
+func IsTag(s string) bool {
+	return s != "" && s[0] == ':'
+}
+
+// IsTag tests whether s starts with a plus or minus sign
+func IsDecl(s string) bool {
+	return s != "" && IsSign(s[0])
+}
+
+// IsExp tests whether t is a non-empty expression tree
+func IsExp(t *Tree) bool {
+	return t.Tok == '(' && len(t.Seq) > 0
+}
+
+// IsSym tests whether t is a non-empty sym token tree
+func IsSym(t *Tree) bool {
+	return t.Tok == Sym && len(t.Val) > 0
+}
+
+// SymPred returns a tree predicated that checks for symbols nested up to depth
+func SymPred(depth int, pred func(string) bool) func(*Tree) bool {
+	return func(t *Tree) bool { _, ok := CheckSym(t, depth, pred); return ok }
+}
+
+// CheckSym checks t or the nested first trees up to depth whether it is a symbol that matches pred
+func CheckSym(t *Tree, depth int, pred func(string) bool) (string, bool) {
+	if IsSym(t) && pred(t.Val) {
+		return t.Val, true
+	} else if depth > 0 && IsExp(t) {
+		return CheckSym(t.Seq[0], depth-1, pred)
+	}
+	return "", false
 }
---
+++ lex/split.go
@@ -0,0 +1,50 @@
+package lex
+
+// Keyed has a list of trees named by a key
+type Keyed struct {
+	*Tree
+	Key string
+	Seq []*Tree
+}
+
+// SplitKeyed returns a list of keyed trees and also the head and tail list of trees
+// It will check for symbols and symbols starting a nested expression.
+// If uni is true each key can be followed by at most one trees otherwise by any number of trees
+func SplitKeyed(seq []*Tree, uni bool, pred func(string) bool) (head, tail []*Tree, res []Keyed) {
+	var last bool
+	head = seq
+	for i, t := range seq {
+		if key, ok := CheckSym(t, 1, pred); ok {
+			if len(res) == 0 {
+				head = seq[:i]
+				res = make([]Keyed, 0, len(seq)-i)
+			}
+			if IsExp(t) {
+				res = append(res, Keyed{t, key, t.Seq[1:]})
+				last = false
+			} else {
+				res = append(res, Keyed{t, key, nil})
+				last = true
+			}
+		} else if last {
+			res[len(res)-1].Seq = seq[i : i+1]
+			last = !uni
+		} else if len(res) > 0 {
+			tail = seq[i:]
+			break
+		}
+	}
+	return
+}
+
+// Split splits seq at the first tree that matches pred and returns both head and tail
+// If the first tree matches, it returns nil, seq; if no tree matches it returns seq, nil;
+// otherwise it returns head without match and tail starting with a match
+func Split(seq []*Tree, pred func(*Tree) bool) (head, tail []*Tree) {
+	for i, t := range seq {
+		if pred(t) {
+			return seq[:i], seq[i:]
+		}
+	}
+	return seq, nil
+}
