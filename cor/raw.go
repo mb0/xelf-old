0893ef2cbbddcda63b7c30fb03502d2102c9add4
Martin Schnabel <mb0@mb0.org>
2019-01-17 02:51:29 +0100

    cor: only format raw bytes as hex string if it is not a plain latin text Latin and latin-1 supplement runes that are not control runes are allowed. This reduces surprises when converting from and to other char literal.

--- cor/raw.go
+++ cor/raw.go
@@ -3,6 +3,9 @@ package cor
 import (
 	"encoding/hex"
 	"strings"
+	"unicode/utf8"
+
+	"github.com/mb0/xelf/lex"
 )
 
 // Raw parses s and returns a pointer to a byte slice or nil on error
@@ -16,6 +19,9 @@ func Raw(s string) *[]byte {
 
 // FormatRaw returns v as a string starting with '\x' and folled by the bytes as lower hex
 func FormatRaw(v []byte) string {
+	if isPlain(v) {
+		return string(v)
+	}
 	var b strings.Builder
 	b.Grow(2 + len(v)*2)
 	b.WriteString(`\x`)
@@ -29,4 +35,16 @@ func ParseRaw(s string) ([]byte, error) {
 		return hex.DecodeString(s[2:])
 	}
 	return []byte(s), nil
+}
+
+// isPlain returns whether b consist only of basic latin and latin-1 supplement non control runes
+func isPlain(b []byte) bool {
+	for len(b) > 0 {
+		r, n := utf8.DecodeRune(b)
+		if !(lex.IsSpace(r) || r > 0x20 && r < 0x7f || r >= 0xad && r <= 0xff) {
+			return false
+		}
+		b = b[n:]
+	}
+	return true
 }
