f2c7f0582fad02dc4b3d74411c1a08c1b43be8b6
Martin Schnabel <mb0@mb0.org>
2019-01-24 22:13:48 +0100

    exp: update package documentation with new parsing rules

--- exp/doc.go
+++ exp/doc.go
@@ -16,11 +16,26 @@ Expression elements are enclosed in parenthesis and usually start with a referen
     Decl: a declaration group that applies to the parent expression
 
 The parsing and resolution process is very abstract and uses following rules:
-Tag and declaration symbols are always treated as sub expression.
-    (eq (e :a 1 :b +d :c 3 - :f) (e (:a 1) (:b) (+d (:c 3)) (:f)))
+Tag and declaration symbols always start an implicit sub expression.
+    (eq (e :a 1 +b 2) (e (:a 1) (+b 2)))
+
+Implicit declarations associate to the elements to the right until the end, another declaration or
+the special naked declaration symbol "-". The elements before the first and after the last
+declaration and the elements inside the declarations are then parsed for tags.
+
+Implicit tags associate to one element right of it unless it is the end, another tag or the special
+naked tag symbol "::". The elements before the first tag are left alone, the ones after the last
+tag are grouped in a tag expression with the special tag "::".
+
+    (eq (e 1 :x 1 +a :y 2 3 +b 4 +c +d 5 - 6 : z 7 8 9)
+        (e 1 (:x 1) (+a (:y 2) (:: 3)) (+b 4) (+c) (+d 5) 6 (:z 7) (:: 8 9)))
+
+In the end a resolver implementation decides whether and how those sub expression are interpreted.
+
 The parser reads literal and type symbols as such and otherwise looks up a resolver by name.
 Expressions starting with a type are rewritten to the 'as'-expression
     (eq (str 'test') (as str 'test'))
+
 Expressions starting with a literal are rewritten to the 'combine'-expression
     (eq ('a' 'b' 'c') (combine 'a' 'b' 'c'))
 
