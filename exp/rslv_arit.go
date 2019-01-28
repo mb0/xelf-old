2750b2831d72397521abfbef3680705fe0132380
Martin Schnabel <mb0@mb0.org>
2019-01-28 17:54:19 +0100

    exp: add arithmetic resolvers add, mul, sub, div and rem.

---
+++ exp/rslv_arit.go
@@ -0,0 +1,167 @@
+package exp
+
+import (
+	"fmt"
+
+	"github.com/mb0/xelf/lit"
+	"github.com/mb0/xelf/typ"
+	"github.com/pkg/errors"
+)
+
+var ErrExpectNumer = errors.New("expected numer argument")
+
+// rslvAdd tries to add up all arguments and converts the sum to the first argument's type.
+func rslvAdd(c *Ctx, env Env, e *Expr) (El, error) {
+	if len(e.Args) == 0 {
+		return lit.Num(0), nil
+	}
+	res, err := resolveNumers(c, env, e.Args, 0, func(r, n float64) float64 { return r + n })
+	if err != nil {
+		return e, err
+	}
+	return convNumerType(e.Args[0], res)
+}
+
+// rslvMul tries to multiply all arguments and converts the product to the first argument's type.
+func rslvMul(c *Ctx, env Env, e *Expr) (El, error) {
+	if len(e.Args) == 0 {
+		return lit.Num(1), nil
+	}
+	res, err := resolveNumers(c, env, e.Args, 1, func(r, n float64) float64 { return r * n })
+	if err != nil {
+		return e, err
+	}
+	return convNumerType(e.Args[0], res)
+}
+
+// rslvSub tries to subtract the sum of the rest from the first argument and
+// converts to the first argument's type.
+func rslvSub(c *Ctx, env Env, e *Expr) (El, error) {
+	err := ArgsMin(e.Args, 2)
+	if err != nil {
+		return nil, err
+	}
+	err = c.ResolveAll(env, e.Args)
+	if err != nil {
+		return e, err
+	}
+	res := getNumer(e.Args[0])
+	if res == nil {
+		return nil, err
+	}
+	sub, err := reduceNumers(e.Args[1:], 0, func(r, n float64) float64 { return r + n })
+	if err != nil {
+		return nil, err
+	}
+	return convNumerType(e.Args[0], lit.Num(res.Num())-sub)
+}
+
+// rslvDiv tries to divide the product of the rest from the first argument.
+// If the first argument is an int div, integer division is used, otherwise it uses float division.
+// The result is converted to the first argument's type.
+func rslvDiv(c *Ctx, env Env, e *Expr) (El, error) {
+	err := ArgsMin(e.Args, 2)
+	if err != nil {
+		return nil, err
+	}
+	err = c.ResolveAll(env, e.Args)
+	if err != nil {
+		return e, err
+	}
+	res := getNumer(e.Args[0])
+	if res == nil {
+		return nil, ErrExpectNumer
+	}
+	div, err := reduceNumers(e.Args[1:], 1, func(r, n float64) float64 { return r * n })
+	if err != nil {
+		return nil, err
+	}
+	if div.IsZero() {
+		return nil, errors.Errorf("zero devision")
+	}
+	if res.Typ().Kind&typ.MaskElem == typ.KindInt {
+		res = lit.Int(res.Num()) / lit.Int(div)
+	} else {
+		res = lit.Num(res.Num()) / div
+	}
+	return convNumerType(e.Args[0], res)
+}
+
+// rslvRem tries to calculate the remainder of the first two arguments and always returns an int.
+func rslvRem(c *Ctx, env Env, e *Expr) (El, error) {
+	err := ArgsExact(e.Args, 2)
+	if err != nil {
+		return nil, err
+	}
+	err = c.ResolveAll(env, e.Args)
+	if err != nil {
+		return e, err
+	}
+	res := getNumer(e.Args[0])
+	if res == nil {
+		return nil, ErrExpectNumer
+	}
+	if err != nil {
+		return nil, err
+	}
+	mod := getNumer(e.Args[1])
+	if mod == nil {
+		return nil, ErrExpectNumer
+	}
+	return lit.Int(res.Num()) % lit.Int(mod.Num()), nil
+}
+
+func convNumerType(e El, r lit.Numer) (El, error) {
+	l, ok := e.(Lit)
+	if ok {
+		n, err := lit.Convert(r, l.Typ(), 0)
+		if err != nil {
+			return nil, err
+		}
+		return n, nil
+	}
+	return nil, fmt.Errorf("%v got %T", ErrExpectNumer, e)
+}
+
+func getNumer(e El) lit.Numer {
+	v, _ := deopt(e).(lit.Numer)
+	return v
+}
+
+func reduceNumers(es []El, res float64, f numerReducer) (lit.Num, error) {
+	for _, arg := range es {
+		v := getNumer(arg)
+		if v == nil {
+			return 0, fmt.Errorf("expected numer argument got %T", arg)
+		}
+		res = f(res, v.Num())
+	}
+	return lit.Num(res), nil
+}
+
+type numerReducer = func(r, e float64) float64
+
+func resolveNumers(c *Ctx, env Env, es []El, res float64, f numerReducer) (lit.Num, error) {
+	err := ArgsForm(es)
+	if err != nil {
+		return 0, err
+	}
+	err = c.ResolveAll(env, es)
+	if err != nil {
+		return 0, err
+	}
+	return reduceNumers(es, res, f)
+}
+
+func deopt(el El) Lit {
+	if l, ok := el.(Lit); ok {
+		if o, ok := l.(lit.Opter); ok {
+			if l = o.Some(); l == nil {
+				t, _ := o.Typ().Deopt()
+				l = lit.Zero(t)
+			}
+		}
+		return l
+	}
+	return nil
+}
