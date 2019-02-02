8a068ff1096766999b7cbc99178d75c848088de4
Martin Schnabel <mb0@mb0.org>
2019-02-02 01:05:25 +0100

    gen add package to generated code for xelf elements

---
+++ gen/gen.go
@@ -0,0 +1,34 @@
+// Package gen provides code generation helpers and specific functions to generate go code.
+package gen
+
+import (
+	"sort"
+
+	"github.com/mb0/xelf/bfr"
+)
+
+// Ctx is the code generation context holding the buffer and addition information.
+type Ctx struct {
+	bfr.B
+	Pkg    string
+	Target string
+	Pkgs   map[string]string
+	Imports
+}
+
+// Imports has a list of alphabetically sorted dependencies. A dependency can be any string
+// recognized by the generator. For go imports the dependency is a package path.
+type Imports struct {
+	List []string
+}
+
+// Add inserts path into the import list if not already present.
+func (i *Imports) Add(path string) {
+	idx := sort.SearchStrings(i.List, path)
+	if idx < len(i.List) && i.List[idx] == path {
+		return
+	}
+	i.List = append(i.List, "")
+	copy(i.List[idx+1:], i.List[idx:])
+	i.List[idx] = path
+}
