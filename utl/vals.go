18be8188091a6b0fe24cf830b7562e80ecd853a1
Martin Schnabel <mb0@mb0.org>
2019-02-12 19:28:50 +0100

    utl: add two useful value resolvers

---
+++ utl/vals.go
@@ -0,0 +1,35 @@
+package utl
+
+import (
+	"github.com/mb0/xelf/exp"
+	"github.com/mb0/xelf/lit"
+	"github.com/mb0/xelf/typ"
+)
+
+// TypedUnresolver will only set enclosed type to the ref or expr to be resolved.
+// This is useful for partial resolution phases where the type is known but the literal isn't.
+type TypedUnresolver struct{ typ.Type }
+
+func (r TypedUnresolver) Resolve(c *exp.Ctx, env exp.Env, e exp.El) (exp.El, error) {
+	switch v := e.(type) {
+	case *exp.Ref:
+		v.Type = r.Type
+	case *exp.Expr:
+		v.Type = r.Type
+	}
+	return e, exp.ErrUnres
+}
+
+// LitResilver will resolve to the enclosed literal. When not used as ref it resolves as dynamic
+// starting with the enclosed literal followed by the expr arguments.
+type LitResolver struct{ lit.Lit }
+
+func (r LitResolver) Resolve(c *exp.Ctx, env exp.Env, e exp.El) (exp.El, error) {
+	switch x := e.(type) {
+	case *exp.Ref:
+		return r.Lit, nil
+	case *exp.Expr:
+		return c.Resolve(env, append(exp.Dyn{r.Lit}, x.Args...))
+	}
+	return e, exp.ErrUnres
+}
