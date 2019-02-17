0065ed3fd15d4b9c9afed1157533a3264461318b
Martin Schnabel <mb0@mb0.org>
2019-02-17 18:05:34 +0100

    utl: change str and time library name to use a colon instead of a dot
    We should reserve the dot '.' exclusively to for interior paths and never
    for declaration names. To make it obvious for all readers where to look.

--- utl/libs.go
+++ utl/libs.go
@@ -6,25 +6,25 @@ import (
 )
 
 var StrLib = Lazy(Fmap{
-	"str.contains":     strings.Contains,
-	"str.contains_any": strings.ContainsAny,
-	"str.prefix":       strings.HasPrefix,
-	"str.suffix":       strings.HasSuffix,
-	"str.upper":        strings.ToUpper,
-	"str.lower":        strings.ToLower,
-	"str.trim":         strings.TrimSpace,
+	"str:contains":     strings.Contains,
+	"str:contains_any": strings.ContainsAny,
+	"str:prefix":       strings.HasPrefix,
+	"str:suffix":       strings.HasSuffix,
+	"str:upper":        strings.ToUpper,
+	"str:lower":        strings.ToLower,
+	"str:trim":         strings.TrimSpace,
 })
 
 var TimeLib = Lazy(Fmap{
-	"time.sub":       time.Time.Sub,
-	"time.add_date":  time.Time.AddDate,
-	"time.add_days":  timeAddDays,
-	"time.year":      time.Time.Year,
-	"time.month":     time.Time.Month,
-	"time.weekday":   time.Time.Weekday,
-	"time.yearday":   time.Time.YearDay,
-	"time.format":    time.Time.Format,
-	"time.date_long": timeDateLong,
+	"time:sub":       time.Time.Sub,
+	"time:add_date":  time.Time.AddDate,
+	"time:add_days":  timeAddDays,
+	"time:year":      time.Time.Year,
+	"time:month":     time.Time.Month,
+	"time:weekday":   time.Time.Weekday,
+	"time:yearday":   time.Time.YearDay,
+	"time:format":    time.Time.Format,
+	"time:date_long": timeDateLong,
 })
 
 func timeAddDays(t time.Time, days int) time.Time { return t.AddDate(0, 0, days) }
