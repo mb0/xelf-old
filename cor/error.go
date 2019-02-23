b3e4ed5df1ae826877f82b4b9119794cb7ea125b
Martin Schnabel <mb0@mb0.org>
2019-02-23 17:03:09 +0100

    cor: add error constructor functions from the xerrors transition package
    also adds a simple string error that does not initialize stack frame info

---
+++ cor/error.go
@@ -0,0 +1,17 @@
+package cor
+
+import "golang.org/x/xerrors"
+
+// centralize access to error api that is going to change in go 1.13
+var (
+	Error  = xerrors.New
+	Errorf = xerrors.Errorf
+)
+
+// StrError returns a simple comparable error value that does not include stack information.
+// This is usefully for lean package level error values.
+func StrError(str string) error { return strError(str) }
+
+type strError string
+
+func (s strError) Error() string { return string(s) }
