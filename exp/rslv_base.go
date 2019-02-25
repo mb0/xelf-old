38270a929101863afd90ded44b927e5d53910ffc
Martin Schnabel <mb0@mb0.org>
2019-02-25 02:25:16 +0100

    exp: move typed unresolver and lit resolver to the exp package

---
+++ exp/rslv_base.go
@@ -0,0 +1,27 @@
+package exp
+
+// TypedUnresolver will only set enclosed type to the ref or expr to be resolved.
+// This is useful for partial resolution phases where the type is known but the literal isn't.
+type TypedUnresolver struct{ Type }
+
+func (r TypedUnresolver) Resolve(c *Ctx, env Env, e El) (El, error) {
+	switch v := e.(type) {
+	case *Ref:
+		v.Type = r.Type
+	case *Expr:
+		v.Type = r.Type
+	}
+	return e, ErrUnres
+}
+
+// LitResilver will resolve to the enclosed literal. When not used as ref it resolves as dynamic
+// starting with the enclosed literal followed by the expr arguments.
+type LitResolver struct{ Lit }
+
+func (r LitResolver) Resolve(c *Ctx, env Env, e El) (El, error) {
+	switch x := e.(type) {
+	case *Expr:
+		return c.Resolve(env, append(Dyn{r.Lit}, x.Args...))
+	}
+	return r.Lit, nil
+}
--- utl/vals.go
+++ utl/vals.go
@@ -1,33 +1,8 @@
 package utl
 
-import (
-	"github.com/mb0/xelf/exp"
-	"github.com/mb0/xelf/lit"
-	"github.com/mb0/xelf/typ"
+import "github.com/mb0/xelf/exp"
+
+type (
+	TypedUnresolver = exp.TypedUnresolver
+	LitResolver     = exp.LitResolver
 )
-
-// TypedUnresolver will only set enclosed type to the ref or expr to be resolved.
-// This is useful for partial resolution phases where the type is known but the literal isn't.
-type TypedUnresolver struct{ typ.Type }
-
-func (r TypedUnresolver) Resolve(c *exp.Ctx, env exp.Env, e exp.El) (exp.El, error) {
-	switch v := e.(type) {
-	case *exp.Ref:
-		v.Type = r.Type
-	case *exp.Expr:
-		v.Type = r.Type
-	}
-	return e, exp.ErrUnres
-}
-
-// LitResilver will resolve to the enclosed literal. When not used as ref it resolves as dynamic
-// starting with the enclosed literal followed by the expr arguments.
-type LitResolver struct{ lit.Lit }
-
-func (r LitResolver) Resolve(c *exp.Ctx, env exp.Env, e exp.El) (exp.El, error) {
-	switch x := e.(type) {
-	case *exp.Expr:
-		return c.Resolve(env, append(exp.Dyn{r.Lit}, x.Args...))
-	}
-	return r.Lit, nil
-}
